36d69b98c502cb172d000cae7ceaf83102188c3f
Andrew <khenkinandrew@gmail.com>
2021-01-17 14:42:52 -0500

    Added another way of doing the bubble sort

--- sorts/BubbleSort.go
+++ sorts/BubbleSort.go
@@ -17,8 +17,32 @@ func bubbleSort(numbers[]int) []int{
 	return numbers
 }
 
+func secondBubbleSort(numbers[]int) []int{
+
+	arrayLength := len(numbers)
+	swapped := true
+
+	for swapped {
+		swapped = false
+		for i := 1; i < arrayLength-1; i++ {
+			if(numbers[i-1] > numbers[i]) {
+				numbers[i], numbers[i-1] = numbers[i-1], numbers[i]
+			}
+		}
+	}
+
+	return numbers
+}
+
+
+
 func main(){
 	var numbers = []int {1, 3, 5, 7, 9, 2, 4, 6, 8, 10}
+	fmt.Println(bubbleSort(numbers))
 	
-	fmt.Println(bubbleSort(numbers))	
+	fmt.Println("----------------------")
+
+	numbers = []int {1, 3, 5, 7, 9, 2, 4, 6, 8, 10}
+	fmt.Println(secondBubbleSort(numbers))
+
 }
